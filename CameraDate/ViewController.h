307a7b9ad0196a8baa3f476f760b250f2a4804e0
KaeJer Cho <moonorblue@gmail.com>
2015-11-06 16:50:25 +0800

    Initial Commit

---
+++ CameraDate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraDate
+//
+//  Created by KaeJer Cho on 2015/11/6.
+//  Copyright © 2015年 KaeJer Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
